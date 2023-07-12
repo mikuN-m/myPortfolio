171cb76446f781a88fc3eb6a9bae230e0e3d85d6
miku <nunon4msmrn@icloud.com>
2023-07-12 14:13:32 +0900

    docs: :memo:

--- portfolio/src/pages/profile.jsx
+++ portfolio/src/pages/profile.jsx
@@ -7,7 +7,27 @@ class Profile extends React.Component {
         return(
             <div>
                 <Header headerLen = {topLen} />
-                <h1>profile</h1>
+                
+                <div className="content js-nav-target">
+
+                    <div className="profile-content">
+                        <div className="profile-name">
+                            <h1>にしまき みく</h1>
+                        </div>
+
+                        <div className="profile-text">
+                            <p>
+                                2003年12月9日生まれ、趣味はソロキャンプとドライブ、映画鑑賞です。<br />
+                                よく作業に行き詰まると車で遠出してリフレッシュしています。
+                                映画はホラーとアクションのジャンルをよく観ています。<br/><br/>
+                                高校生の時、「プログラミングができたらかっこいいのでは」と思ったことがきっかけで勉強を始めました。<br/>
+                                途中何度か挫折を経験しましたが、今では新しいことを学ぶのがとても楽しくなっています。<br/>
+                                まだまだ未熟ですが、これからも沢山の事を学んでいきたいと思っています。
+                            </p>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
         )
     }
--- portfolio/src/pages/skill.jsx
+++ portfolio/src/pages/skill.jsx
@@ -4,10 +4,53 @@ import Header from "../components/header";
 class Skill extends React.Component {
     render(){
         const skillLen = ['no','no','now','no'];
+
+        const skillBox = [
+            ['HTML','見出しや段落、リスト、フォームなどの基本的な記述ができます。'],
+
+            ['CSS',`サイトの外観のレイアウトや、アニメーションの設定、カラムレイアウトの作成ができます。
+            現在はbootstrapなどのフレームワークなどについて勉強中です。`],
+
+            ['JavaScript',`ユーザーの操作に応じてイベントリスナーを実行することができます。
+                            for文、if文、関数やクラスなど基本的な構文をかけます。
+                            Node.jsについても、セッションを用いてログイン、ログアウトなどのユーザー管理、
+                            SQLとの接続と操作を行うことができます。
+                            現在はReactやVue.jsなどのフレームワークについて勉強中です。`],
+
+            ['Git','Githubとの接続やcommit、pushなどの初歩的な操作ができます。'],
+        ];
+
+        const skillList = [];
+
+        skillBox.forEach(item => {
+            skillList.push(
+                <div className="skill-box">
+                    <div className="skill-name">
+                        <h1>&nbsp;&nbsp;{item[0]}</h1>
+                    </div>
+                    <div className="skill-text">
+                        <p>
+                            {item[1]}
+                        </p>
+                    </div>
+                </div>
+            )
+            
+        })
+
         return(
             <div>
                 <Header headerLen = {skillLen} />
-                <h1>skill</h1>
+                
+                <div className="content js-nav-target">
+
+                    <div className="skill-content">
+
+                        {skillList}
+
+                    </div>
+                    
+                </div>
             </div>
         )
     }
