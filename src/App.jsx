87d0e324c95f554ba75819af46cc4adc96d5aa3e
miku <nunon4msmrn@icloud.com>
2023-07-18 12:27:13 +0900

    app.jsx更新

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ class App extends React.Component {
   render(){
     return(
       <div>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <Routes>
             <Route path="/" element={<Top />} />
             <Route path="/profile" element={<Profile />} />
